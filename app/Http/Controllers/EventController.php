59b0e1da6b7d90b062c6b76be49651460cbbfa18
Elton Bongue <eltonbongue@gmail.com>
2024-02-15 22:19:00 +0100

    testando a conexao com a BD e fazendo alguns testes

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -3,10 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Evento;
 
 class EventController extends Controller
 {
     public function index(){
+
+        $eventos = Evento::all();
+        return view('welcome', ['eventos' => $eventos]);
 
     }
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,6 +8,13 @@
      
     <img src="/img/kite.jpg" width="100%" height="500px" alt="">
 
+    @foreach ($eventos as $evento)
+
+    <p>{{$evento->title}}</p>
+    <p>{{$evento->descricao}}</p>
+        
+    @endforeach
+
     @endsection
 
      
