9462c83c8c9ab9cd736ed404368cd257419d5f47
Elton Bongue <lourencoelton874@gmail.com>
2024-09-11 00:44:03 +0100

    alterando o layout da home

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,22 +5,47 @@
 
     @section('content')
 
+
+
+
+
+    <div style="background: url(../img/ttt.png); height: 450px;">
+      <div class="flex items-center justify-center h-full mx-auto px-4 ">
+          <div>
+              <div class="text-center"> 
+                  <h1 class="text-gray-100 text-3xl font-bold">Já ouviu falar sobre a <strong>Kitestudio?</strong></h1>
+                  <p>conheça os serviços do melhor estúdio de Luanda</p>
+              </div>
+              
+          
+          
+
+          
+          <form action="" class="mt-7 space-x-4">
+              
+            <button class="inline-flex items-center text-yellow-900 bg-orange-500 py-2 px-6 hover:bg-gray-800 rounded">
+              serviços
+            </button>
+
+            <button class="inline-flex items-center text-yellow-900 bg-orange-500 py-2 px-6 hover:bg-gray-800 rounded">
+              sobre
+            </button>
+            
+              <button class="inline-flex items-center text-yellow-900 bg-orange-500 py-2 px-6 hover:bg-gray-800  rounded"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-3 ">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+                </svg>
+                  novidades
+                </button>
+
+
+
+          </form>
+      </div>
+      </div>
+  </div>
    
 
-     <div id="carouselExampleSlidesOnly" class="carousel slide mt-10" data-bs-ride="carousel">
-                <div class="carousel-inner">
-                  <div class="carousel-item active">
-                    <img src="/img/foto1.jpg" class="d-block w-100 h-96"  alt="...">
-                  </div>
-                  <div class="carousel-item ">
-                    <img src="/img/foto2.jpg" class="d-block w-100 h-96" alt="...">
-                  </div>
-                  <div class="carousel-item">
-                    <img src="/img/foto3.jpg" class="d-block w-100 h-96" alt="...">
-                  </div>
-                </div>
-              </div>
-
+    
         
 
             
